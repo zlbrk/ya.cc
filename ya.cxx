07b275c9125a41536e48872b38e3f3abefb78076
zlbrk <>
2025-03-31 08:44:40 +0300

    Last Sync: 2025-03-31 05:44:40 (Mobile)

---
+++ ya.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	string name;
+	cout << "type name: ";
+	cin >> name;
+	cout << "Hello, " << name << "!\n";
+	
+	return 0;
+}
