afa8ee062b792a9446a7fe896967df214622e145
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2025-04-05 23:15:56 +0300

    Контейнер std::forward_list
    Это экономичный список, в который элементы можно добавлять
    или в начало fl.push_front() или после указанного итератора.
    Пример показывает, как можно "заменить" элемент в конце такого списка с помощью fl.insert_after(), предварительно удалив
    элемент за текущим итератором с помощью fl.erase_after().
    
    В приведённом примере после удаления предпоследнего из четырёх
    элементов на его место перемещается последний элемент, а сам
    итератор не сдвигается, как это происходит в списке std::list.

--- ya.cc
+++ ya.cc
@@ -1,45 +1,21 @@
+#include <forward_list>
 #include <iostream>
-#include <list>
-
-void printli (const char* list_name, const std::list<int>& l) {
-  std::cout << list_name << ": [";
-  for (int i : l) {
-    std::cout << i << ", ";
-  }
-  std::cout << "\b\b]\n";
-}
+#include <iterator>
 
 int main() {
-  std::list<int> l = {0, 10, 15, 20};
-  // печатаем оригинальный список
-  printli("orig", l);
-  // вместо этого можно воспользоваться std::next(l.begin())
-  // auto iter = l.begin();
-  // ++iter;
-  auto iter = std::next(l.begin());
-  l.insert(iter, 5);  // вставляем на эту позицию элемент
-
-  // печатаем обновлённый список
-  printli("mod1", l);
-  std::advance(iter, -2);
-  std::cout << "*iter after std::advance(iter, -2): " << *iter << "\n";
-
-  // Удаляем из списка чётные числа
-  for (auto iter = l.begin(); iter != l.end(); ) {
-    if (*iter % 2 == 0) {
-      iter = l.erase(iter);  // возвращается итератор на элемент, следующий за удалённым
-    } else {
-      ++iter;
+    std::forward_list<int> fl = {3, 42, 5};
+    fl.push_front(2);
+    // fl.push_back(10);  // ошибка компиляции!
+    for (int x : fl) {
+        std::cout << x << "\n";  // 2 3 42 5
     }
-  }
-  printli("mod2", l);
-  auto iter2 = l.begin();
+    std::cout << "\n";
+    auto iter1 = std::next(fl.begin());
+    // auto iter2 = std::next(fl.end());
+    iter1 = fl.erase_after(iter1);
+    fl.insert_after(iter1, 100500);
 
-  std::cout << "*iter2 before std::advance(iter2, 1): " << *iter2 << "\n";
-
-  std::advance(iter2, 1);
-
-  std::cout << "*iter2 after std::advance(iter2, 1): " << *iter2 << "\n";
-
-  return 0;
+    for (int x : fl) {
+        std::cout << x << "\n";  // 2 3 5 4
+    }
 }
