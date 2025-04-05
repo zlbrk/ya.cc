d113b9d719df7b6c99cf613ee5b5d3c92789f72d
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2025-04-05 14:56:26 +0300

    Пример с итераторами
    С итераторами стандартных контейнеров мы познакомимся подробнее в других параграфах. Заметим пока, что к итераторам списка (в отличие от итераторов вектора) нельзя прибавлять произвольные целые числа, чтобы сдвинуть их на несколько позиций вперёд: эта операция не может быть реализована эффективно за константное время.

--- ya.cc
+++ ya.cc
@@ -1,35 +1,20 @@
 #include <iostream>
-#include <vector>
+#include <list>
 
-using namespace std;
+int main() {
+  std::list<int> l = {0, 10, 15, 20};
 
-void PrintVS(const char* name, vector<string> vs) {
-  cout << name;
-  for (string s : vs) {
-    cout << s << ", ";
+  auto iter = l.begin();
+  ++iter;
+  l.insert(iter, 5);  // вставляем на эту позицию элемент
+
+  // Удаляем из списка чётные числа
+  for (auto iter = l.begin(); iter != l.end(); ) {
+    if (*iter % 2 == 0) {
+      iter = l.erase(iter);  // возвращается итератор на элемент, следующий за удалённым
+    } else {
+      ++iter;
+    }
   }
-  cout << "\b\b!\n";
-}
-
-void Swap(vector<string>& s1, vector<string>& s2) {
-  vector<string> temp = s1;
-  s1 = s2;
-  s2 = temp;
-}
-
-int main () {
-  vector<string> words1 = {"one", "two", "three"};
-  vector<string> words2 = {"tree", "two", "one"};
-
-  cout << "Before Swap(words1, words2)" << "\n";
-  PrintVS("words1: ", words1);
-  PrintVS("words2: ", words2);
-
-  Swap(words1, words2);
-
-  cout << "After Swap(words1, words2)" << "\n";
-  PrintVS("words1: ", words1);
-  PrintVS("words2: ", words2);
-
   return 0;
 }
