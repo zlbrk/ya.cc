1e1e592955f3d1be8060a12e3b2320fc9c538139
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2025-04-06 00:10:18 +0300

    Инвалидация итераторов и ссылок
    Если в результате операции с контейнером произошла реаллокация,
    например, при добавлении элемента в конец вектора, то
    итераторы, указатели и ссылки, определённые ранее, нельзя
    использовать.

--- ya.cc
+++ ya.cc
@@ -1,29 +1,17 @@
-#include <forward_list>
 #include <iostream>
-#include <iterator>
+#include <list>
 
 int main() {
-    std::forward_list<int> fl = {3, 42, 5};
-    fl.push_front(2);
-    // fl.push_back(10);  // ошибка компиляции!
-    for (int x : fl) {
-        std::cout << x << "\n";  // 2 3 42 5
-    }
-    std::cout << "\n";
-    auto iter1 = std::next(fl.begin());
-    iter1 = fl.erase_after(iter1);
-    fl.push_front(1);
-    // 1 2 3 5
+    std::list<int> v = {1, 2, 3, 4}; // сначала это был вектор
 
-    auto iter2 = std::next(fl.begin());
-    iter2 = fl.erase_after(std::next(iter2));
-    // fl.insert_after(std::next(iter2), 4);
-    fl.push_front(0);
-    // Покраска кубика Рубика
-    for (int x : fl) {
-      x = x + 1;
-      std::cout << x << "\n";  // 1 2 3 5
-    }
+    auto iter = v.begin();  // итератор
+    int* ptr = &v.front();  // указатель
+    int& ref = v.front();  // ссылка
 
+    std::cout << *iter << " " << *ptr << " " << ref << "\n";  // 1 1 1
 
+    v.push_back(5);  // происходит реаллокация
+
+    // обращаться к старым итераторам, указателям и ссылкам больше нельзя:
+    std::cout << *iter << " " << *ptr << " " << ref << "\n";  // неопределённое поведение!
 }
