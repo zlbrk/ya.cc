2a3f0995ca1a661b16c5af44cd63dc31a3c0a394
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2025-04-15 18:28:09 +0300

    Сложный момент. надо проработать с Гигачатом
    Здесь создаётся временный вектор для сортировки несортированного отображения слов в частоты употребления.

--- ya.cc
+++ ya.cc
@@ -1,28 +1,43 @@
+#include <algorithm>
 #include <iostream>
-#include <map>
 #include <string>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
 
 int main() {
-  // инициализируем map набором пар {ключ, значение}
-  std::map<std::string, int> years = {
-    {"Moscow", 1147},
-    {"Rome", -753},
-    {"London", 47},
-  };
 
-  for (const auto& [city, year] : years) {
-    std::cout << city << ": " << year << "\n";
+  std::unordered_map<std::string, int> freqs;
+  // freqs.reserve(300'000);  // можно использовать апостроф для выделения разрядов
+  std::string word;
+
+  while (std::cin >> word) {
+    ++freqs[word]; // вставка считанного слова в контейнер
+    // так как повторная вставка ключа перезаписывает
+    // его значение, то таким нехитрым образом мы узнаем
+    // сколько раз одно и то же слово встретилось в файле
   }
 
-  std::cout << "\nwithout auto\n";
+  // копируем пары в вектор, используя шаблонный конструктор от двух итераторов:
+  std::vector<std::pair<std::string, int>> sortedByFreq(
+    freqs.begin(),
+    freqs.end()
+  );
 
-  // если бы не было автовыведения типов, то
-  // пришлось бы писать полностью
-  for (const std::pair<const std::string, int>& item : years) {
-    // иными словами, в примере элементами контейнера map
-    // являются пары типа std::pair<const std::string, int>,
-    //
-    std::cout << item.first << ": " << item.second << "\n";
+  // сортируем с помощью своей лямбда-функции:
+  std::sort(
+    sortedByFreq.begin(),
+    sortedByFreq.end(),
+    [](const auto& p1, const auto& p2) {
+      // сначала сравниваем частоты по убыванию, потом - слова по возрастанию
+      return std::tie(p2.second, p1.first) < std::tie(p1.second, p2.first);
+    }
+  );
+
+
+
+  for (const auto& [word, freq] : sortedByFreq) {
+    std::cout << word << "\t" << freq << "\n";
   }
   return 0;
 }
