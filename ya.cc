bd49ed16cdb0501b689c14d160fa923718f469a4
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2025-04-19 09:16:40 +0300

    std::set и std::unordered_set
    ищем дубликаты слов в файле не
    самым лучшим способом

--- ya.cc
+++ ya.cc
@@ -1,43 +1,29 @@
-#include <algorithm>
 #include <iostream>
+#include <set>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <vector>
+#include <unordered_set>
+
 
 int main() {
+  // здесь будем хранить все слова (каждое по одному разу)
+  std::unordered_set<std::string> words;
+  // здесь будем хранить повторяющиеся слова
+  // используем set, чтобы потом напечатать их
+  // по алфавиту
+  std::set<std::string> duplicate_words;
 
-  std::unordered_map<std::string, int> freqs;
-  // freqs.reserve(300'000);  // можно использовать апостроф для выделения разрядов
   std::string word;
-
   while (std::cin >> word) {
-    ++freqs[word]; // вставка считанного слова в контейнер
-    // так как повторная вставка ключа перезаписывает
-    // его значение, то таким нехитрым образом мы узнаем
-    // сколько раз одно и то же слово встретилось в файле
+    if (words.contains(word)) {
+      duplicate_words.insert(word);
+    } else {
+      words.insert(word);
+    }
   }
 
-  // копируем пары в вектор, используя шаблонный конструктор от двух итераторов:
-  std::vector<std::pair<std::string, int>> sortedByFreq(
-    freqs.begin(),
-    freqs.end()
-  );
 
-  // сортируем с помощью своей лямбда-функции:
-  std::sort(
-    sortedByFreq.begin(),
-    sortedByFreq.end(),
-    [](const auto& p1, const auto& p2) {
-      // сначала сравниваем частоты по убыванию, потом - слова по возрастанию
-      return std::tie(p2.second, p1.first) < std::tie(p1.second, p2.first);
-    }
-  );
-
-
-
-  for (const auto& [word, freq] : sortedByFreq) {
-    std::cout << word << "\t" << freq << "\n";
+  for (const auto& word : duplicate_words) {
+    std::cout << word << "\n";
   }
   return 0;
 }
