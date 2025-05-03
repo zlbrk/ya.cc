32baf9f32bb8ed7baa6e48891da533820d95cbd5
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2025-05-03 23:25:12 +0300

    Сдвиги вперёд-назад по итератору
    if (auto nextIter = std::next(iter); nextIter != numbers.end())
    вот странноватый if, но можно разобраться
    с ним позже

--- ya.cc
+++ ya.cc
@@ -1,22 +1,48 @@
 #include <iostream>
+#include <iterator>
 #include <map>
+#include <string>
 
+// Итераторы ассоциативных контейнеров
+// контейнеры map и set и их мультиверсии 
+// предоставляют итераторы, которые можно сдвигать
+// на соседние позиции вперёд и назад 
 
 int main() {
-  // здесь будем хранить все слова (каждое по одному разу)
-  std::multimap<std::string, int> positions;
+  std::map<int, std::string> numbers = {
+    {100, "hundred"},
+    {3, "three"},
+    {42, "forty two"},
+    {11, "eleven"},
+  };
   
-  std::string word;
-  int position = 0;
+  auto iter = numbers.find(11);
     
-  while (std::cin >> word) {
-    positions.insert({word, position});
-    ++position;
+  if (iter != numbers.end()) {
+    // печатаем найденный элемент
+    const auto& [key, value] = *iter;
+    std::cout << "Found: " << key << ": " << value << "\n";
+    // Found: 11: eleven
+
+    // печатаем предыдущий элемент
+    if (iter != numbers.begin()) {
+      const auto& [key, value] = *std::prev(iter);
+      std::cout << "Previous: " << key << ": " << value << "\n";
+      // Previous: 3: three
+    } else {
+      std::cout << "No previous element\n";
+    }
+
+    // печатаем следующий элемент
+    if (auto nextIter = std::next(iter); nextIter != numbers.end()) {
+      const auto& [key, value] = *nextIter;
+      std::cout << "Next: " << key << ": " << value << "\n";
+    } else {
+      std::cout << "No next element\n";
+    }
+  } else {
+    std::cout << "Not found\n";
   }
 
-
-  for (const auto& [word, pos] : positions) {
-    std::cout << word << "\t" << pos << "\n";
-  }
   return 0;
 }
