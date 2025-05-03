16dba8958c2728f0e9dc2d38250ff7eb759c14ac
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2025-05-03 22:11:59 +0300

    std::multimap - это один из четырёх мультиконтейнеров
    здесь могут дублироваться ключи, поэтому
    можно наполнить такой контейнер парами
    <слово, позиция>, например.

--- ya.cc
+++ ya.cc
@@ -1,32 +1,22 @@
 #include <iostream>
-#include <set>
-#include <string>
-#include <unordered_set>
+#include <map>
 
 
 int main() {
   // здесь будем хранить все слова (каждое по одному разу)
-  std::unordered_set<std::string> words;
+  std::multimap<std::string, int> positions;
   
-  // здесь будем хранить повторяющиеся слова
-  // используем set, чтобы потом напечатать их
-  // по алфавиту
-  std::set<std::string> duplicate_words;
-  
-  // переменная, в которую считывается очередное слово
-  // из входного потока
   std::string word;
-  
+  int position = 0;
+    
   while (std::cin >> word) {
-    auto [iter, has_been_inserted] = words.insert(word);
-    if (!has_been_inserted) {
-      duplicate_words.insert(word);
-    }
+    positions.insert({word, position});
+    ++position;
   }
 
 
-  for (const auto& word : duplicate_words) {
-    std::cout << word << "\n";
+  for (const auto& [word, pos] : positions) {
+    std::cout << word << "\t" << pos << "\n";
   }
   return 0;
 }
