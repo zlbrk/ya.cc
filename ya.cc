dcc61d2ea303cfb0488cd726586c2aa04311e626
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2025-05-08 19:28:31 +0300

    Демонстрация различий между алгоритмами и встроенными функциями
    Общая идея, заложенная в дизайн стандартной библиотеки C++: если некоторый алгоритм имеет более эффективную реализацию для конкретного контейнера, то он будет реализован у этого контейнера в виде встроенной функции. Наоборот, если возможна лишь общая реализация, то специальной встроенной функции не будет. Поэтому, например, у вектора и дека нет своей встроенной функции find.

--- ya.cc
+++ ya.cc
@@ -1,33 +1,34 @@
 #include <algorithm>
 #include <iostream>
-#include <deque>
+#include <set>
 #include <npkit.hh>
 
 // using namespace np;
 
-// пример применения ф-ции std::find
+// Демонстрация различий 
+// между алгоритмами и встроенными функциями
 int main() {
-  std::deque<int> di = {1, 2, 3, 4, 3, 2, 1, 0};
-  std::deque<std::string> ds = {"one", "two", "three", "two", "one"};
-  np::printc("vi", di);
-  np::printc("vs", ds);
-  std::cout << std::count(di.begin(), di.end(), 8) << "\n";
-  std::cout << np::count(ds.begin(), ds.end(), "one") << "\n";
-  auto iter1 = std::find(di.begin(), di.end(), 4);
-  // итераторы дека можно вычитать, напечатается индекс
-  // найденного элемента
-  // std::cout << (iter1 - di.begin()) << "\n";
-  std::cout << "" << (iter1 - di.begin()) << "\n";
+  std::set<int> numbers = {2, 3, 5, 7, 11, 13, 17, 19};
+  // std::set<std::string> ds = {"one", "two", "three", "two", "one"};
+  np::printc("numbers", numbers);
 
-  // к итераторам дека и вектора можно прибавлять целые числа
-  auto start = ds.begin();
-  auto end = start + 3;
-  auto iter2 = std::find(start, end, "five");
-  auto iter3 = np::find(start, end, "three");
-  if (iter2 == end) {
-    std::cout << "No such element!\n";
+  //Предпочтительный способ
+  auto iter1 = numbers.find(19);
+
+  if (iter1 != numbers.end()) {
+    std::cout << *iter1 << " found at " << &*iter1 << "\n";
   } else {
-    std::cout << *iter2 << "\n";
+    std::cout << "Not found!\n";
   }
-  std::cout << *iter3 << "\n";
+
+  // Допустимо, но неэффективно!
+  auto iter2 = std::find(numbers.begin(), numbers.end(), 11);
+
+  if (iter2 != numbers.end()) {
+    std::cout << *iter2 << " found at " << &*iter2 << "\n";
+  } else {
+    std::cout << "Not found!\n";
+  }
+  std::cout << "Diff: " << &*iter1 - &*iter1 << "\n";  
+
 }
